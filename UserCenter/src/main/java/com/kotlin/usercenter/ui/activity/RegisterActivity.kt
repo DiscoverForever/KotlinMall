7a15bab84634e60117473f087ef6eed097be2ad8
tangyueyang <yueyangtang@163.com>
2018-10-31 16:57:24 +0800

    Mod mvp搭建

--- UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -2,12 +2,19 @@ package com.kotlin.usercenter.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.kotlin.usercenter.R
+import kotlinx.android.synthetic.main.activity_register.*
+//import org.jetbrains.anko.toast
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        mRegisterBtn.setOnClickListener {
+//            toast("test")
+            Toast.makeText(this, "注册", Toast.LENGTH_SHORT).show()
+        }
     }
 }
